1a0580221266600a4d84d5da7a7fb5c02a25451b
muhmooda <44399821+muhmooda@users.noreply.github.com>
2018-10-23 12:33:46 +0100

    textadventure

---
+++ TextAdventure.py
@@ -0,0 +1,18 @@
+print("----------4000CEM Week 4 Lab 2 Task----------")
+print("----------------<InsertTitleHere>---------------")
+print("---------------<InsertAuthorsHere>--------------")
+print("\n")
+
+print("You awake in a dark room.  Do you:")
+print("a) Scream for help.")
+print("b) Press the light switch")
+x = input("Enter a or b: ")
+if x == "a":
+    print("Someone hears your screams...")
+    # Contine adventure Here
+elif x == "b":
+    print("The light comes on.")
+    print("You do not recognise the room but you have a really bad feeling...")
+    # Contine adventure Here
+else:
+    print("That was not an option.  Game Over")
